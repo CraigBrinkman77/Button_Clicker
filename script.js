d6805558988528af5f65925c96163312925c2c26
Craig Brinkman <craigfrancis77@gmail.com>
2021-11-14 16:23:44 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function change(element){
+    element.innerText = "Sign Out"
+}
+function change2(){
+    alert("Ninja was Liked")
+}
+function disappear(){
+    document.getElementById("appear").innerHTML= "<br />" +"Definition Given";
+    document.getElementById("gone").innerHTML= "";
+}
